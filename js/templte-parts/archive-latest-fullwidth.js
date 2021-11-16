48a89f6e23e435c599077711410911b2762ded16
Alexey-Geyvan <alexey.geyvan@kawalee.com>
2021-11-16 17:22:47 +0200

    Slider Fixes

--- js/templte-parts/archive-latest-fullwidth.js
+++ js/templte-parts/archive-latest-fullwidth.js
@@ -2,7 +2,12 @@
 $(document).ready(function () {    
     const swiperLatestFullwidth = new Swiper(".latest-news__slider", {
       spaceBetween: 30,
+      speed: 600,
       slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 4000,
+      },
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
--- js/templte-parts/archive-top-articles.js
+++ js/templte-parts/archive-top-articles.js
@@ -1,11 +1,53 @@
 
-$(document).ready(function () {    
-    const swiperTopArticles = new Swiper(".top-articles__slider", {
-      spaceBetween: 30,
-      slidesPerView: 4,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      }          
-    });
+$(document).ready(function () { 
+
+  var swiperTopArticles = Swiper;
+  var init = false;
+
+  function swiperMode() {
+      let mobile = window.matchMedia('(min-width: 0px) and (max-width: 768px)');
+      let desktop = window.matchMedia('(min-width: 769px)');
+    // Enable (for mobile)
+    if(mobile.matches) {
+        swiper.destroy();
+        init = false;
+    } else {
+      if (!init) {
+        init = true;
+        var swiperTopArticles = new Swiper(".top-articles__slider", {
+          spaceBetween: 30,
+          slidesPerView: 2,
+          speed: 600,
+          loop: true,
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          breakpoints: {   
+            992: {
+              slidesPerView: 3,
+              spaceBetween: 30
+            },
+            1200: {
+              slidesPerView: 4,
+              spaceBetween: 30
+            }
+          }          
+        });
+      }
+    }
+  }
+
+  /* On Load
+  **************************************************************/
+  window.addEventListener('load', function() {
+  swiperMode();
+  });
+
+  /* On Resize
+  **************************************************************/
+  window.addEventListener('resize', function() {
+  swiperMode();
+  });
+
 });
